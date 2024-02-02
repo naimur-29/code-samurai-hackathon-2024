52bfc7a220d2738da615439c3ffc87cf946e0394
Naim <skylike2904@gmail.com>
2024-02-02 18:56:30 +0600

    up

--- controller/getBooks.js
+++ controller/getBooks.js
@@ -2,14 +2,31 @@ const BookModel = require("../models/book");
 
 const getBooks = async (req, res) => {
   try {
-    let response = {
-      books: [],
+    let query = {};
+    let sortCriteria = {};
+
+    const { title, author, genre, sort, order } = req.query;
+
+    if (title) query.title = title;
+    if (author) query.author = author;
+    if (genre) query.genre = genre;
+
+    if (sort) {
+      const validSortingFields = ["title", "author", "genre", "price"];
+      if (!validSortingFields.includes(sort)) {
+        return res.status(400).json({ error: "Invalid sorting field" });
+      }
+
+      sortCriteria[sort] = order === "desc" ? -1 : 1;
+    } else {
+      sortCriteria._id = 1;
     }
-    response.books = await BookModel.find();
-    response.books.sort((a, b) => b.id - a.id);
-    res.status(200).json(response);
+
+    const books = await BookModel.find(query).sort(sortCriteria);
+
+    res.status(200).json({ books: books });
   } catch (error) {
-    res.status(404).json({error: error.message});
+    res.status(404).json({ error: error.message });
   }
 };
 
