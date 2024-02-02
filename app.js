67470ac258ba98b3fb6b9822ec3af0df9442c685
Naim <skylike2904@gmail.com>
2024-02-02 17:38:33 +0600

    up

--- app.js
+++ app.js
@@ -4,14 +4,11 @@ const connectDb = require("./Db");
 const bookRoutes = require("./routes/book");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/mydatabase", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect("mongodb://localhost:27017/mydatabase")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
