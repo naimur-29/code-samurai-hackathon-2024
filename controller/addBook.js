5ff4f63e52e092c0c0f05435b8785cf86956c617
Naim <skylike2904@gmail.com>
2024-02-02 18:36:45 +0600

    up

--- controller/addBook.js
+++ controller/addBook.js
@@ -2,11 +2,12 @@ const BookModel = require("../models/book");
 
 const addBook = async (req, res) => {
   try {
-    console.log(req.body);
-    const data = await BookModel.create(req.body);
-    console.log(data);
+    let booksCount = await BookModel.find();
+    booksCount = booksCount.length;
+    const data = {...req.body, id: booksCount + 1};
+    const newBook = await BookModel.create(data);
 
-    res.status(201).json(data);
+    res.status(201).json(newBook);
   } catch (error) {
     res.send({ error });
     console.log(error);
